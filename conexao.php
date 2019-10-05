fc765757f88eff3cc775fb8de6ee157773025033
micaeleee <49121243+micaeleee@users.noreply.github.com>
2019-10-05 15:30:58 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -5,5 +5,5 @@
 	$dbname = "funildevendas";
 	
 	//Criar a conexão
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+	$conn = @mysqli_connect($servidor, $usuario, $senha, $dbname);
 ?>
