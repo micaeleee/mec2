99ddba5c6971002eed499a4cfbec48325ca7c467
micaeleee <49121243+micaeleee@users.noreply.github.com>
2019-10-05 15:32:04 -0300

    Update salva_mensagem.php

--- salva_mensagem.php
+++ salva_mensagem.php
@@ -6,5 +6,5 @@
 	$mensagem = $_POST['mensagem'];
 	
 	$result_msg_contato = "INSERT INTO mensagens_contatos(nome, email, assunto, mensagem, created) VALUES ('$nome', '$email', '$assunto', '$mensagem', NOW())";
-	$resultado_msg_contato= mysqli_query($conn, $result_msg_contato)
+	$resultado_msg_contato= @mysqli_query($conn, $result_msg_contato)
 ?>
